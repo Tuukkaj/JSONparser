12c646a3693cd2937d2a5d8daddb05d68735cb45
Tuukkaj <juusela.tuukka@gmail.com>
2018-11-05 20:33:17 +0200

    Add JSONObject.

--- src/main/java/JSONParser.java
+++ src/main/java/JSONParser.java
@@ -4,6 +4,6 @@ class JSONParser {
 
     public static void main(String... args) {
         System.out.println("Author: Tuukka Juusela <tuukka.juusela@cs.tamk.fi>");
-        writer = new JSONWriter();
+
     }
 }
