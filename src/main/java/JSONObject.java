991ad29ee471f2c43a232c5252388f9c7b53befd
Tuukkaj <juusela.tuukka@gmail.com>
2018-11-06 10:41:00 +0200

    JSONObject: Add javadoc.

--- src/main/java/JSONObject.java
+++ src/main/java/JSONObject.java
@@ -1,5 +1,18 @@
 abstract class JSONObject {
+    /**
+     * Key of the object in JSONFile.
+     */
     String key;
+
+    /**
+     * Builds JSONObjects data to String format.
+     * @return
+     */
     abstract String buildToString();
+
+    /**
+     * Retuns key of the JSON Object.
+     * @return key.
+     */
     abstract String getKey();
 }
