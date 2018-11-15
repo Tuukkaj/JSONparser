cabaa1a5dbcbfca499d2f6b91f1149bef6244e07
Tuukkaj <juusela.tuukka@gmail.com>
2018-11-15 16:18:46 +0200

    JSONObject: Add javadoc and remove unnecessary constructor.

--- src/main/java/JSONComponent/JSONObject.java
+++ src/main/java/JSONComponent/JSONObject.java
@@ -2,35 +2,70 @@ package JSONComponent;
 
 import java.util.LinkedHashMap;
 
+/**
+ * @author      Tuukka Juusela <tuukka.juusela@cs.tamk.fi>
+ * @version     2018.1115
+ * @since       1.8
+ *
+ * JSONObject which can store JSONItems, JSONArrays and other JSONObjects.
+ * Used by JSONFileData.
+ */
 public class JSONObject extends JSONComponent {
+    /**
+     * Holds all of key pairs and data for the JSONObject.
+     */
     private LinkedHashMap<String, Object> table;
 
-    public JSONObject() {
-        table = new LinkedHashMap<>();
-    }
-
+    /**
+     * Constructor of the class. Sets Objects key.
+     */
     public JSONObject(String key) {
         table = new LinkedHashMap<>();
         setKey(key);
     }
 
+    /**
+     * Returns key's value from JSONObject's LinkedHashMap.
+     * @param key to get value from LinkedHashMap.
+     * @return
+     */
     public Object getObject(String key) {
         return table.get(key);
     }
 
+    /**
+     * Adds new key and value to LinkedHashMap.
+     *
+     * Puts item's key and data to LinkedHashMap.
+     * @param item to add.
+     */
     public void add(JSONItem item) {
         table.put(item.getKey(), item.getData());
     }
 
-
+    /**
+     * Removes key's value from JSONObject's LinkedHashMap.
+     * @param key
+     */
     public void remove(String key) {
         table.remove(key);
     }
 
+    /**
+     * Returns JSONObjects LinkedHashMap.
+     * @return JSONObjects LinkedHashMap.
+     */
     public LinkedHashMap<String, Object> getData() {
         return table;
     }
 
+    /**
+     * Builds JSONObject's data to be written to a file.
+     *
+     *  Goes through JSONObject's data and converts it to String. Determines if the data is JSONComponent, String or a
+     *  some other Object.
+     * @return Ready to write String for JSONFileData to use.
+     */
     @Override
     public String buildToString() {
         String space = "  ";
