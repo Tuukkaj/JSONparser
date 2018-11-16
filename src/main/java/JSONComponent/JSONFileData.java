742db80f83932174b962d575e6b77adb385d3ded
Tuukkaj <juusela.tuukka@gmail.com>
2018-11-16 11:28:07 +0200

    JSONFileData: Add Javadoc.

--- src/main/java/JSONComponent/JSONFileData.java
+++ src/main/java/JSONComponent/JSONFileData.java
@@ -5,13 +5,30 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+/**
+ * @author      Tuukka Juusela <tuukka.juusela@cs.tamk.fi>
+ * @version     2018.1116
+ * @since       1.8
+ *
+ * Holds information of the JSON file.
+ */
 public class JSONFileData {
+    /**
+     * LinkedHashMap of keys and JSONComponents. Holds all of the JSONItem's, JSONObject's and JSONArray of the JSONFile.
+     */
     LinkedHashMap<String, JSONComponent> map;
 
+    /**
+     * Constructor of the class. Initializes JSONFileData's LinkedHashMap.
+     */
     public JSONFileData() {
         map = new LinkedHashMap<>();
     }
 
+    /**
+     * Goes through JSONFileData's LinkedHashMap and creates ArrayList of JSONComponents.
+     * @return ArrayList<JSONComponent> from JSONFileData's LinkedHashMap.
+     */
     public ArrayList<JSONComponent> getComponents() {
         ArrayList<JSONComponent> componentList = new ArrayList<>();
         for(String s: map.keySet()) {
@@ -20,18 +37,37 @@ public class JSONFileData {
         return componentList;
     }
 
+    /**
+     * Gets key value from JSONFileData's LinkedHashMap.
+     * @param key to get data with.
+     * @return JSONComponent from JSONFileData's LinkedHashMap.
+     */
     public JSONComponent getComponent(String key) {
         return map.get(key);
     }
 
+    /**
+     * Adds new JSONComponent to JSONFileData's LinkedHashMap.
+     * @param component to add.
+     */
     public void add(JSONComponent component) {
         map.put(component.getKey(), component);
     }
 
+    /**
+     * Removes key's value from JSONFileData's LinkedHashMap.
+     * @param key to remove value.
+     */
     public void remove(String key) {
         map.remove(key);
     }
 
+    /**
+     * Builds JSONFileData's LinkedHashMap to JSON file.
+     *
+     * Calls LinkedHashMap's JSONComponents and builds the file using their buildToString() -method.
+     * @return ready to write/print JSONFile with indentations.
+     */
     public String buildToString() {
         String space = "  ";
         StringBuilder b = new StringBuilder("{\n");
@@ -51,6 +87,14 @@ public class JSONFileData {
         return indent(b.toString());
     }
 
+    /**
+     * Indents JSONFile's file.
+     *
+     * Transforms parameter to ArrayList by splitting param with "\n". Checks where "{" or "[" begins
+     * and where "}" or "]" ends and indents accordingly.
+     * @param textFile file to indent.
+     * @return indented file.
+     */
     private String indent(String textFile) {
         int indentLevel = 0;
         String space = "  ";
