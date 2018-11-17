eb0c8644544f6b9d0f2b7c8828001e41e47a1550
tuukkaj <juusela.tuukka@gmail.com>
2018-11-17 11:39:51 +0200

    JSONComponentTest: Add tests for JSONObject and JSONArray.

--- src/test/java/JSONComponentTest.java
+++ src/test/java/JSONComponentTest.java
@@ -1,5 +1,10 @@
+import JSONComponent.JSONArray;
+import JSONComponent.JSONObject;
 import org.junit.*;
 import JSONComponent.JSONItem;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class JSONComponentTest {
 
@@ -14,5 +19,47 @@ public class JSONComponentTest {
         Assert.assertEquals(nullItem.getData(), null);
         Assert.assertEquals(intItem.getData(), 12);
         Assert.assertEquals(objectItem.getData(), intItem);
+    }
+
+    @Test
+    public void JSONObjectTest() {
+        JSONObject testObject = new JSONObject("testObject");
+        JSONObject innerObject = new JSONObject("testInnerObject");
+        JSONArray innerArray = new JSONArray("testInnerArray");
+
+        testObject.add(new JSONItem("intTest", -26));
+        testObject.add(new JSONItem("nullTest", null));
+        testObject.add(new JSONItem("stringTest", "STRING TEST"));
+        testObject.add(new JSONItem("objectTest", innerObject));
+        testObject.add(new JSONItem("arrayTest", innerArray));
+
+        Assert.assertEquals(testObject.getObject("intTest"),-26);
+        Assert.assertEquals(testObject.getObject("nullTest"),null);
+        Assert.assertEquals(testObject.getObject("stringTest"),"STRING TEST");
+        Assert.assertEquals(testObject.getObject("objectTest"),innerObject);
+        Assert.assertEquals(testObject.getObject("arrayTest"),innerArray);
+    }
+
+    @Test
+    public void JSONArrayTest() {
+        JSONArray testArray = new JSONArray("testArray");
+        JSONObject innerObject = new JSONObject("testInnerObject");
+        JSONArray innerArray = new JSONArray("testInnerArray");
+        ArrayList<JSONItem> itemList = new ArrayList<>();
+
+        itemList.add(new JSONItem("intTest", -5437812));
+        itemList.add(new JSONItem("nullTest", null));
+        itemList.add(new JSONItem("stringTest", "STRING TEST"));
+        itemList.add(new JSONItem("objectTest", innerObject));
+        itemList.add(new JSONItem("arrayTest", innerArray));
+
+        testArray.add(itemList);
+
+        LinkedHashMap testLinkedHashMap = testArray.getLinkedHashMap().get(0);
+        Assert.assertEquals(testLinkedHashMap.get("intTest"),-5437812);
+        Assert.assertEquals(testLinkedHashMap.get("nullTest"),null);
+        Assert.assertEquals(testLinkedHashMap.get("stringTest"),"STRING TEST");
+        Assert.assertEquals(testLinkedHashMap.get("objectTest"),innerObject);
+        Assert.assertEquals(testLinkedHashMap.get("arrayTest"),innerArray);
     }
 }
