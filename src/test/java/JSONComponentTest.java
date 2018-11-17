622e5e27284f80906b2be8df2a96169b9e4a4ed8
tuukkaj <juusela.tuukka@gmail.com>
2018-11-17 11:28:13 +0200

    JSONComponentTest: Add test for JSONItem.

--- src/test/java/JSONComponentTest.java
+++ src/test/java/JSONComponentTest.java
@@ -3,5 +3,16 @@ import JSONComponent.JSONItem;
 
 public class JSONComponentTest {
 
+    @Test
+    public void JSONItemTest() {
+        JSONItem nullItem = new JSONItem("nullItem", null);
+        JSONItem stringItem = new JSONItem("stringItem", "testString");
+        JSONItem intItem = new JSONItem("intItem", 12);
+        JSONItem objectItem = new JSONItem("objectItem", intItem);
 
+        Assert.assertEquals(stringItem.getData(),"testString");
+        Assert.assertEquals(nullItem.getData(), null);
+        Assert.assertEquals(intItem.getData(), 12);
+        Assert.assertEquals(objectItem.getData(), intItem);
+    }
 }
