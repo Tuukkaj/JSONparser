152a9a6f3eaa382c26124950ae6a9ced8a8779ed
tuukkaj <juusela.tuukka@gmail.com>
2018-11-18 11:08:12 +0200

    JSONReadingTest: Add test for correctly reading exampleJSON.json.

--- src/test/java/JSONReadingTest.java
+++ src/test/java/JSONReadingTest.java
@@ -9,6 +9,21 @@ public class JSONReadingTest {
     public static JSONReader reader;
     public static JSONFileData fd;
 
+    @Test
+    public void testDataGetting() {
+        Assert.assertEquals(((JSONItem)fd.getComponent("firstName")).getData(), "John");
+        Assert.assertEquals(((JSONItem)fd.getComponent("lastName")).getData(), "Smith");
+        Assert.assertEquals(((JSONItem)fd.getComponent("isAlive")).getData(), true);
+        Assert.assertEquals(((JSONItem)fd.getComponent("age")).getData(), 27);
+        Assert.assertEquals(((JSONObject)fd.getComponent("address")).getObject("city"), "New York");
+        Assert.assertEquals(((JSONObject)fd.getComponent("address")).getObject("state"), "NY");
+        Assert.assertEquals(((JSONArray)fd.getComponent("phoneNumbers")).getLinkedHashMap().get(0).get("type"), "home");
+        Assert.assertEquals(((JSONArray)fd.getComponent("phoneNumbers")).getLinkedHashMap().get(1).get("number"), "646 555-4567");
+        Assert.assertEquals(((JSONArray)fd.getComponent("phoneNumbers")).getLinkedHashMap().get(2).get("type"), "mobile");
+        Assert.assertEquals(((JSONArray)fd.getComponent("phoneNumbers")).getLinkedHashMap().get(2).get("type"), "mobile");
+        Assert.assertEquals(((JSONItem)fd.getComponent("spouse")).getData(), null);
+        Assert.assertTrue(fd.getComponent("children") instanceof JSONArray);
+    }
 
     @BeforeClass
     public static void beforeClass() {
