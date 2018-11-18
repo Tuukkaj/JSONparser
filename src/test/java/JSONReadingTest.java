8144f90b2d0df2ca882ac150fe6084c9ddb55f12
tuukkaj <juusela.tuukka@gmail.com>
2018-11-18 11:06:30 +0200

    JSONReadingTest: Add JSONReader and JSONFileData as variables.

--- src/test/java/JSONReadingTest.java
+++ src/test/java/JSONReadingTest.java
@@ -1,5 +1,18 @@
 import JSONComponent.*;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
 
 public class JSONReadingTest {
+    public static JSONReader reader;
+    public static JSONFileData fd;
+
+
+    @BeforeClass
+    public static void beforeClass() {
+        reader = new JSONReader();
+        fd = reader.readFile(new File("src/test/java/exampleJSON.json"));
+    }
 }
