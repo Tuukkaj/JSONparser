ef69be546f5dec3bb4395cf803911a0b6489dbed
Tuukkaj <juusela.tuukka@gmail.com>
2018-11-19 11:08:20 +0200

    JSONReadingTest: Spread firstTest to 3 different methods.

--- src/test/java/JSONReadingTest.java
+++ src/test/java/JSONReadingTest.java
@@ -28,19 +28,26 @@ public class JSONReadingTest {
      * Test for getting data from firstTest.
      */
     @Test
-    public void firstTest() {
-        Assert.assertEquals(((JSONItem) firstTest.getComponent("firstName")).getData(), "John");
-        Assert.assertEquals(((JSONItem) firstTest.getComponent("lastName")).getData(), "Smith");
-        Assert.assertEquals(((JSONItem) firstTest.getComponent("isAlive")).getData(), true);
-        Assert.assertEquals(((JSONItem) firstTest.getComponent("age")).getData(), 27);
-        Assert.assertEquals(((JSONObject) firstTest.getComponent("address")).getObject("city"), "New York");
-        Assert.assertEquals(((JSONObject) firstTest.getComponent("address")).getObject("state"), "NY");
+    public void firstTestArrays() {
         Assert.assertEquals(((JSONArray) firstTest.getComponent("phoneNumbers")).getLinkedHashMap().get(0).get("type"), "home");
         Assert.assertEquals(((JSONArray) firstTest.getComponent("phoneNumbers")).getLinkedHashMap().get(1).get("number"), "646 555-4567");
         Assert.assertEquals(((JSONArray) firstTest.getComponent("phoneNumbers")).getLinkedHashMap().get(2).get("type"), "mobile");
         Assert.assertEquals(((JSONArray) firstTest.getComponent("phoneNumbers")).getLinkedHashMap().get(2).get("type"), "mobile");
-        Assert.assertEquals(((JSONItem) firstTest.getComponent("spouse")).getData(), null);
         Assert.assertTrue(firstTest.getComponent("children") instanceof JSONArray);
+    }
+
+    @Test
+    public void firstTestItems() {
+        Assert.assertEquals(((JSONItem) firstTest.getComponent("firstName")).getData(), "John");
+        Assert.assertEquals(((JSONItem) firstTest.getComponent("lastName")).getData(), "Smith");
+        Assert.assertEquals(((JSONItem) firstTest.getComponent("isAlive")).getData(), true);
+        Assert.assertEquals(((JSONItem) firstTest.getComponent("age")).getData(), 27);
+        Assert.assertEquals(((JSONItem) firstTest.getComponent("spouse")).getData(), null);
+    }
+
+    @Test public void firstTestObjects() {
+        Assert.assertEquals(((JSONObject) firstTest.getComponent("address")).getObject("city"), "New York");
+        Assert.assertEquals(((JSONObject) firstTest.getComponent("address")).getObject("state"), "NY");
     }
 
     @Test
