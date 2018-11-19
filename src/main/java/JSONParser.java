a8d2b96c32c7c5cc43331a64533b93a6ccb5a485
Tuukkaj <juusela.tuukka@gmail.com>
2018-11-19 17:19:28 +0200

    JSONParser: Add Javadoc.

--- src/main/java/JSONParser.java
+++ src/main/java/JSONParser.java
@@ -7,10 +7,29 @@ import java.util.LinkedHashMap;
 
 import JSONComponent.JSONArray;
 
+/**
+ * Main class of the program. Holds methods for reading and writing JSON files. Also holds methods for creation of
+ * JSONComponents that are part of the JSONFileData.
+ *
+ * @author Tuukka Juusela
+ * @version 2018.1911
+ * @since 1.8
+ */
 class JSONParser {
+    /**
+     * JSONReader used to read JSON files.
+     */
     private static JSONReader reader;
+
+    /**
+     * JSONWriter used to write JSON files.
+     */
     private static JSONWriter writer;
 
+    /**
+     * Main method. Prints authors name and creates JSONWriter and JSONReader.
+     * @param args command line argument. Not in use.
+     */
     public static void main(String... args) {
         System.out.println("Author: Tuukka Juusela <tuukka.juusela@cs.tamk.fi>");
 
@@ -75,30 +94,67 @@ class JSONParser {
         //writer.write(temp, new File(("JSONWritingTests/test2.json")));
     }
 
+    /**
+     * Creates new JSONItem from parameters.
+     *
+     * @param key of JSONItem.
+     * @param data of JSONItem. String, Boolean, Int, Float and null.
+     * @return JSONItem created from parameters.
+     */
     public JSONItem createJSONItem(String key, Object data) {
         return new JSONItem(key,data);
     }
 
+    /**
+     * Creates new JSONObject with key from parameter.
+     *
+     * @param key of JSONObject.
+     * @return JSONObject created with key from parameter.
+     */
     public JSONObject createJSONObject(String key) {
         return new JSONObject(key);
     }
 
+    /**
+     * Creates new JSONArray with key from parameter.
+     * @param key of JSONArray.
+     * @return JSONArray created with key from parameter.
+     */
     public JSONArray createJSONArray(String key) {
         return new JSONArray(key);
     }
 
+    /**
+     * Creates JSONFileData where JSONComponents are inserted.
+     * @return new JSONFileData.
+     */
     public JSONFileData createJSONFileData() {
         return new JSONFileData();
     }
 
+    /**
+     * Writes JSONFileData's data to given file.
+     *
+     * @param fileData JSONFileData to write.
+     * @param fileToWriteOn file to write on.
+     */
     public void write(JSONFileData fileData, File fileToWriteOn) {
         writer.write(fileData, fileToWriteOn);
     }
 
+    /**
+     * Prints JSONFileData to console. Simulates writing a file.
+     * @param fileData JSONFileData to print.
+     */
     public void print(JSONFileData fileData) {
         writer.print(fileData);
     }
 
+    /**
+     * Reads File location and converts it to JSONFileData.
+     * @param file to read.
+     * @return JSONFileData created from file.
+     */
     public JSONFileData read(File file) {
         return reader.readFile(file);
     }
